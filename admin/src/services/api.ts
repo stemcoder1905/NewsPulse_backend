cce2a605eade910cac318a2ace6ff466611f581e
Deepak Kumar Mishra <dm01888@gmail.com>
2026-09-10 17:44:53 +0530

    Prepare frontend for deployment

--- admin/src/services/api.ts
+++ admin/src/services/api.ts
@@ -1,7 +1,8 @@
+
 import axios from 'axios';
 
 export const adminApi = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/admin',
+  baseURL: import.meta.env.VITE_API_URL + '/api/v1/admin',
   headers: {
     'Content-Type': 'application/json'
   }
